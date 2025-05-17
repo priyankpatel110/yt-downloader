b23ffed7ae3deeb2490158048b27be0e3c44a9e6
PriyankHere <patelpriyankhere@gmail.com>
2025-05-17 20:16:29 +0530

    Add Render-compatible port setup

--- app.py
+++ app.py
@@ -46,3 +46,10 @@ def favicon():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
